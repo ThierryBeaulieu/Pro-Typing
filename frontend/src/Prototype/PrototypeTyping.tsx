1acd0685978a67d923a43639fc11c7267d5ec3b8
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-02-27 00:01:33 -0500

    Quick prototype

--- frontend/src/Prototype/PrototypeTyping.tsx
+++ frontend/src/Prototype/PrototypeTyping.tsx
@@ -10,14 +10,23 @@ import '@fontsource/roboto/700.css';
 
 function PrototypeTyping() {
   const [userInput, setUserInput] = useState('');
+  const [correctChar, setCorrectChar] = useState<boolean[]>([]);
   const text = dummyText['short-content'].split('');
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === 'Backspace') {
         setUserInput((prev) => prev.slice(0, -1));
+        setCorrectChar((prev) => prev.slice(0, -1));
       } else if (event.key.length === 1) {
+        if (text.length === correctChar.length) return;
         setUserInput((prev) => prev + event.key);
+
+        if (text[correctChar.length] === event.key) {
+          setCorrectChar((prev) => [...prev, true]);
+        } else {
+          setCorrectChar((prev) => [...prev, false]);
+        }
       }
     };
 
@@ -25,15 +34,27 @@ function PrototypeTyping() {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
+  }, [correctChar.length, text]);
   console.log({ userInput });
+  console.log(correctChar.length);
+  console.log(text.length);
+
+  const isCorrectChar = (index: number) => {
+    if (correctChar[index] === true) return 'correct';
+    if (correctChar[index] === false) return 'incorrect';
+    return '';
+  };
+
+  const isActiveChar = (index: number) => {
+    return userInput.length === index ? 'active' : '';
+  };
 
   return (
     <div className="paragraph">
       {text.map((letter: string, index: number) => (
         <div className="tile-wrapper">
           <span
-            className={`tile ${userInput.length == index ? 'active' : ''} `}
+            className={`tile ${isCorrectChar(index)} ${isActiveChar(index)}`}
             key={index}
           >
             {letter}
