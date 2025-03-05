e43c343231e566a84e89f5c3ab28aa64e2b2f6d8
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-05 11:39:15 -0500

    reconfigure the routing to redirect buttons to the correct amount of words per minute

--- frontend/src/Components/CertificationButtons.tsx
+++ frontend/src/Components/CertificationButtons.tsx
@@ -1,5 +1,6 @@
 import { Box, Button } from '@mui/material';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface CertificationButtonsProp {
   wordsPerMinute: number[];
@@ -7,8 +8,14 @@ interface CertificationButtonsProp {
 
 function CertificationButtons({ wordsPerMinute }: CertificationButtonsProp) {
   const [state, setState] = useState(true);
+  const navigate = useNavigate();
+
   const handleClick = () => {
     setState(!state);
+  };
+
+  const handleCertificationClick = (wpm: number) => {
+    navigate(`/certification/${wpm}`);
   };
 
   return (
@@ -20,7 +27,11 @@ function CertificationButtons({ wordsPerMinute }: CertificationButtonsProp) {
           </Button>
         ) : (
           wordsPerMinute.map((wpm, index) => (
-            <Button key={index} variant="outlined" onClick={handleClick}>
+            <Button
+              key={index}
+              variant="outlined"
+              onClick={() => handleCertificationClick(wpm)}
+            >
               {wpm} wpm
             </Button>
           ))
