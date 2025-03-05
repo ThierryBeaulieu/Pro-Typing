f765db331c2cdcab461ada21b4b87dff8b943ac7
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-05 13:06:39 -0500

    add typing content in the certification page

--- frontend/src/Components/CertificationMenu.tsx
+++ frontend/src/Components/CertificationMenu.tsx
@@ -1,17 +1,9 @@
-
-import certifications from '../interfaces/MajorCertifications';
+import TypingContent from './TypingContent';
 
 function CertificationMenu() {
-
   return (
     <>
-      {certifications[0].subCertifications.map((subCertification, cIndex) => (
-        <div className="certification-description" key={cIndex}>
-          <h2>{subCertification.name}</h2>
-          <h3>{subCertification.range}</h3>
-          <p>{subCertification.description}</p>
-        </div>
-      ))}
+      <TypingContent />
     </>
   );
 }
