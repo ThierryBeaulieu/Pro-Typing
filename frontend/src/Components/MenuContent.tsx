81ad1dab8ca3d0107993fb8a7ba4d0bc9c808f87
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-09 15:14:02 -0400

    made the animation loop to attract attention

--- frontend/src/Components/MenuContent.tsx
+++ frontend/src/Components/MenuContent.tsx
@@ -1,114 +1,106 @@
 import Box from '@mui/material/Box/Box';
-import typingVideo from '../assets/home-page-animation.mp4';
+import typingVideo from '../assets/home-page-animation-2.mp4';
 import Grid2 from '@mui/material/Grid2/Grid2';
 import rocketImage from '../assets/space-man.jpeg';
 import drivingImage from '../assets/driving-man.jpeg';
 import Button from '@mui/material/Button/Button';
 import { useNavigate } from 'react-router';
 
-function MenuContent() {
+function ImageWrapper({ imageName }: { imageName: string }) {
+  return (
+    <Box
+      width={'100%'}
+      component="img"
+      alt="certification-image"
+      src={imageName}
+      sx={{ borderRadius: '30px' }}
+    />
+  );
+}
+
+type TextWrapperProps = {
+  path: string;
+  text: string;
+  buttonTxt: string;
+};
+
+function TextWrapper({ path, text, buttonTxt }: TextWrapperProps) {
   const navigate = useNavigate();
 
-  const navigateToTraining = () => {
-    navigate('/training');
+  const navigateToPath = () => {
+    navigate(path);
   };
 
-  const navigateToCertification = () => {
-    navigate('/certifications');
-  };
   return (
     <>
-      <Grid2 container spacing={4} margin={4}>
-        <Grid2 size={{ xs: 12, sm: 6, md: 3, lg: 3 }}>
-          <Box
-            width={'100%'}
-            component="img"
-            alt="certification-image"
-            src={drivingImage}
-            sx={{ borderRadius: '30px' }}
-          />
-        </Grid2>
-        <Grid2
-          size={{ xs: 12, sm: 6, md: 3, lg: 3 }}
-          sx={{
-            background: 'linear-gradient(135deg, #00bbe0, #ea4549)',
-            borderRadius: '30px',
+      <Grid2
+        size={{ xs: 12, sm: 6, md: 3, lg: 3 }}
+        sx={{
+          borderRadius: '30px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          padding: 4,
+        }}
+      >
+        <h3
+          style={{
             display: 'flex',
             justifyContent: 'center',
+            textAlign: 'center',
             alignContent: 'center',
             alignItems: 'center',
-            flexDirection: 'column',
-            padding: 4,
           }}
         >
-          <h3
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              textAlign: 'center',
-              alignContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            Show you impressive skills to your future employer
-          </h3>
-          <Button
-            onClick={navigateToCertification}
-            sx={{ mt: 3 }}
-            variant="contained"
-          >
-            Get Certified
-          </Button>
-        </Grid2>
-        <Grid2 size={{ xs: 12, sm: 6, md: 3, lg: 3 }}>
-          <Box
-            width={'100%'}
-            component="img"
-            alt="certification-image"
-            src={rocketImage}
-            sx={{ borderRadius: '30px' }}
-          />
-        </Grid2>
-        <Grid2
-          size={{ xs: 12, sm: 6, md: 3, lg: 3 }}
-          sx={{
-            backgroundColor: '#081221',
-            borderRadius: '30px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            padding: 4,
-          }}
-        >
-          <h3
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              textAlign: 'center',
-              alignContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            Reach a speed higher than you could ever imagine
-          </h3>
-          <Button
-            onClick={navigateToTraining}
-            sx={{ mt: 3 }}
-            variant="contained"
-          >
-            Start Training
-          </Button>
-        </Grid2>
+          {text}
+        </h3>
+        <Button onClick={navigateToPath} sx={{ mt: 3 }} variant="contained">
+          {buttonTxt}
+        </Button>
       </Grid2>
+    </>
+  );
+}
 
-      <Box sx={{ width: '100%', maxWidth: 800, margin: '0 auto' }}>
-        <video autoPlay muted playsInline style={{ width: '100%' }}>
+function MenuContent() {
+  return (
+    <>
+      <Box
+        sx={{
+          width: { xs: '90%', sm: '80%', md: '70%', lg: '60%', xl: '70%' },
+          margin: { xs: '5%', sm: '10%', md: 'auto' },
+          maxWidth: 800,
+        }}
+      >
+        <video autoPlay muted playsInline loop style={{ width: '100%' }}>
           <source src={typingVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </Box>
+
+      <Grid2 container spacing={4} margin={4}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 3, lg: 3 }}>
+          <ImageWrapper imageName={drivingImage} />
+        </Grid2>
+
+        <TextWrapper
+          path={'/training'}
+          text={'Reach a speed higher than you could ever imagine'}
+          buttonTxt={'Start Training'}
+        />
+
+        <Grid2 size={{ xs: 12, sm: 6, md: 3, lg: 3 }}>
+          <ImageWrapper imageName={rocketImage} />
+        </Grid2>
+
+        <TextWrapper
+          path={'/certifications'}
+          text={'Show you impressive skills to your future employer'}
+          buttonTxt={'Get Certified'}
+        />
+      </Grid2>
     </>
   );
 }
