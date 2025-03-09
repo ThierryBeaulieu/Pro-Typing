040833686a3bd482b6782645131375db6f719c97
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-09 13:45:27 -0400

    adjust indentation for readibility

--- frontend/src/Pages/MenuPage.tsx
+++ frontend/src/Pages/MenuPage.tsx
@@ -5,13 +5,13 @@ import typingVideo from '../assets/home-page-animation.mp4'
 function MenuPage() {
   return (
     <>
-      <NavigationBar />
-      <Box sx={{ width: '100%', maxWidth: 800, margin: '0 auto' }}>
-        <video autoPlay muted playsInline style={{ width: '100%'}}>
-            <source src={typingVideo} type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
-    </Box>
+        <NavigationBar />
+        <Box sx={{ width: '100%', maxWidth: 800, margin: '0 auto' }}>
+            <video autoPlay muted playsInline style={{ width: '100%'}}>
+                <source src={typingVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </Box>
     </>
   );
 }
