5fa9a99a67cb2c64063a2af7fec4a79e2188fedc
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-09 14:10:53 -0400

    add right text so that it's easy to read main page, but it's not easy to read on larger screen

--- frontend/src/Pages/MenuPage.tsx
+++ frontend/src/Pages/MenuPage.tsx
@@ -3,33 +3,43 @@ import NavigationBar from '../Components/NavigationBar';
 import typingVideo from '../assets/home-page-animation.mp4'
 import Grid2 from '@mui/material/Grid2/Grid2';
 import rocketImage from '../assets/space-man.jpeg';
+import Button from '@mui/material/Button/Button';
 
 function MenuPage () {
   return (
     <>
       <NavigationBar />
+      <Grid2 container spacing={4} margin={4}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 6 }}>
+          <Box
+            width={'100%'}
+            component="img"
+            alt="certification-image"
+            src={rocketImage}
+            sx={{ borderRadius: '30px' }}
+          />
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 6 }}
+          sx={{
+            backgroundColor: '#081221',
+            borderRadius: '30px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            padding: 4
+          }}>
+          <h3 style={{ display: 'flex', justifyContent: 'center', textAlign: 'center', alignContent: 'center', alignItems: 'center' }}>Reach a speed higher than you could ever imagine</h3>
+          <Button sx={{mt: 3}} variant='contained'>Get certified</Button>
+        </Grid2>
+      </Grid2>
       <Box sx={{ width: '100%', maxWidth: 800, margin: '0 auto' }}>
         <video autoPlay muted playsInline style={{ width: '100%' }}>
           <source src={typingVideo} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </Box>
-
-      <Grid2 container spacing={4} >
-        <Grid2 size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 3 }}>
-          <h1>Reach a speed higher than you could ever imagine</h1>
-        </Grid2>
-        <Grid2 size={{ xs: 12, sm: 6, md: 6, lg: 6, xl: 6 }}>
-          <Box
-            width={'100%'}
-            component="img"
-            alt="certification-image"
-            src={rocketImage}
-          />
-
-        </Grid2>
-
-      </Grid2>
     </>
   );
 }
