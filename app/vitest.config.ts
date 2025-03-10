6a2f35ee4c0f606fe71ff8abe020bcf84f6c31cc
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-10 11:38:23 -0400

    add elemnts to ignore in coverage

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -3,6 +3,16 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      exclude: [
+        '**.config.**',
+        'src/interfaces/**',
+        'src/utils/**',
+        'src/enum/**',
+        'dist/**',
+        'src/main.tsx',
+        'src/app.tsx',
+        'src/vite-env.d.ts',
+      ],
       thresholds: {
         lines: 80,
         functions: 80,
