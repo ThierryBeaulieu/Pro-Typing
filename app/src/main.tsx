464ccc1b5be1d9afcb738631e80f987d308d179b
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-10 11:19:03 -0400

    rename app so that it can be used as lower case

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './app';
 import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
