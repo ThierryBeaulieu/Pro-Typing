55de05fc281be06534fca09393490f02f0109efe
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-11 19:20:21 -0400

    remove weird description

--- app/src/pages/LandingPage/LandingPageContent.tsx
+++ app/src/pages/LandingPage/LandingPageContent.tsx
@@ -94,22 +94,6 @@ function MenuContent() {
       </Box>
 
       <Grid2 container spacing={4} margin={4}>
-        <Grid2 size={{ xs: 12, sm: 12, md: 12, lg: 12 }}>
-          <h1>Get certified</h1>
-          <ul>
-            <li>Our brand new brand new AI powered engine ensures that you</li>
-            <li>
-              Take our AI powered certifications and show to the world that you
-              can type fast.
-            </li>
-            <li>
-              Get the edge during the screening process by having our
-              certifications.
-            </li>
-          </ul>
-          <p></p>
-        </Grid2>
-
         <Grid2 size={{ xs: 12, sm: 6, md: 3, lg: 3 }}>
           <ImageWrapper imageName={rocketImage} />
         </Grid2>
