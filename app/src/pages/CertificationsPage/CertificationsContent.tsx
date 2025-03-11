ddb491cc9d4fc097a2aa7c461d79eb301ef3eab1
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-11 19:03:35 -0400

    use the term Complete again because it's more clear that the user compelted the certification

--- app/src/pages/CertificationsPage/CertificationsContent.tsx
+++ app/src/pages/CertificationsPage/CertificationsContent.tsx
@@ -71,7 +71,7 @@ function CertificationCard({ image, data }: CertificationCardType) {
               variant="contained"
               onClick={() => handleCertificationClick(data.wordsPerMinute)}
             >
-              Take Again
+              Complete again
             </Button>
           ) : (
             <Button
