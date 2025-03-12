d403d02737d55342f4a3ba221dd2e3ab67c386ad
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-11 20:10:43 -0400

    add good description

--- app/src/pages/LandingPage/LandingPageContent.tsx
+++ app/src/pages/LandingPage/LandingPageContent.tsx
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box/Box';
 import Grid2 from '@mui/material/Grid2/Grid2';
 import rocketImage from '../../assets/certifications-type/space-man.jpeg';
 import drivingImage from '../../assets/certifications-type/driving-man.jpeg';
+import appDescription from '../../assets/app-description.png';
 import Button from '@mui/material/Button/Button';
 import { useNavigate } from 'react-router';
 import { Typography } from '@mui/material';
@@ -9,10 +10,11 @@ import { Typography } from '@mui/material';
 type TextWrapperProps = {
   path: string;
   text: string;
-  buttonTxt: string;
+  buttonTxt?: string;
+  size: object;
 };
 
-function TextWrapper({ path, text, buttonTxt }: TextWrapperProps) {
+function TextWrapper({ path, text, buttonTxt, size }: TextWrapperProps) {
   const navigate = useNavigate();
 
   const navigateToPath = () => {
@@ -22,7 +24,7 @@ function TextWrapper({ path, text, buttonTxt }: TextWrapperProps) {
   return (
     <>
       <Grid2
-        size={{ xs: 12, sm: 6, md: 3, lg: 3 }}
+        size={size}
         sx={{
           borderRadius: '30px',
           display: 'flex',
@@ -44,9 +46,49 @@ function TextWrapper({ path, text, buttonTxt }: TextWrapperProps) {
         >
           {text}
         </h3>
-        <Button onClick={navigateToPath} sx={{ mt: 3 }} variant="contained">
-          {buttonTxt}
-        </Button>
+        {buttonTxt !== undefined ? (
+          <Button onClick={navigateToPath} sx={{ mt: 3 }} variant="contained">
+            {buttonTxt}
+          </Button>
+        ) : (
+          <></>
+        )}
+      </Grid2>
+    </>
+  );
+}
+
+function TextWrapperDescription({ size }: { size: object }) {
+  return (
+    <>
+      <Grid2
+        size={size}
+        sx={{
+          borderRadius: '30px',
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          gap: 2,
+          padding: 4,
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: {
+              xs: '24px',
+              sm: '30px',
+              md: '36px',
+              lg: '42px',
+              xl: '50px',
+            },
+          }}
+        >
+          Certify your typing speed
+        </Typography>
+        <Typography>
+          The average typing speed is around 40 words per minute, fast typing
+          speeds could give you an edge in the interviewing process.
+        </Typography>
       </Grid2>
     </>
   );
@@ -78,6 +120,7 @@ function MenuContent() {
       >
         <Typography
           sx={{
+            fontWeight: 'bold',
             fontSize: {
               xs: '26px',
               sm: '36px',
@@ -93,12 +136,21 @@ function MenuContent() {
 
       <Grid2 container spacing={4} margin={4}>
         <Grid2 size={{ xs: 12, sm: 6, md: 3, lg: 3 }}>
+          <ImageWrapper imageName={appDescription} />
+        </Grid2>
+
+        <TextWrapperDescription size={{ xs: 12, sm: 6, md: 9, lg: 9 }} />
+
+        <Grid2 size={{ xs: 12, sm: 6, md: 3, lg: 3 }}>
           <ImageWrapper imageName={rocketImage} />
         </Grid2>
 
         <TextWrapper
+          size={{ xs: 12, sm: 6, md: 3, lg: 3 }}
           path={'/certifications'}
-          text={'Show your impressive skills to your future employer'}
+          text={
+            'Get a certification with our state of the art AI engin that prevents cheating'
+          }
           buttonTxt={'Get Certified'}
         />
 
@@ -107,6 +159,7 @@ function MenuContent() {
         </Grid2>
 
         <TextWrapper
+          size={{ xs: 12, sm: 6, md: 3, lg: 3 }}
           path={'/training'}
           text={'Reach a speed higher than you could ever imagine'}
           buttonTxt={'Start Training'}
