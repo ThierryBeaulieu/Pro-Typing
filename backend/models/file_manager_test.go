19ff68ebba8a39351950028e766b27ec18332efd
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-18 19:29:59 -0400

    following learn go with tests approach for TDD

--- backend/models/file_manager_test.go
+++ backend/models/file_manager_test.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -10,35 +9,14 @@ func TestFileManager(t *testing.T) {
 	t.Run("Fetch all certifications", func(t *testing.T) {
 
 		fileManager := FileManager{}
-		var got []Certification = fileManager.FetchAllCertifications()
 
-		var want []Certification = []Certification{}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
 	})
 
 	t.Run("Fetch all certifications completed", func(t *testing.T) {
 
-		fileManager := FileManager{}
-		var got []CertificationComplete = fileManager.FetchAllCertificationsCompleted()
-		var want []CertificationComplete = []CertificationComplete{}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
 	})
 
 	t.Run("Fetch a specified certifications", func(t *testing.T) {
 
-		id := ""
-		fileManager := FileManager{}
-		var got *Certification = fileManager.FetchCertification(id)
-		var want *Certification = nil
-
-		if got != want {
-			t.Errorf("got %v want %v, given %s", got, want, id)
-		}
 	})
 }
