f84c339a728573cad8a6c04ee3b1289855f09356
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-18 21:23:11 -0400

    add comment of the website I'm using

--- backend/main.go
+++ backend/main.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 )
 
+// continue
+// https://quii.gitbook.io/learn-go-with-tests/build-an-application/http-server
+
 func main() {
 	handler := http.HandlerFunc(CertificationServer)
 	log.Fatal(http.ListenAndServe(":5000", handler))
