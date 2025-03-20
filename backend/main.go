f000303d8a6179fde45e4780dcbd9124bdab0220
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-20 01:54:29 -0400

    add exemple of adapter pattern

--- backend/main.go
+++ backend/main.go
@@ -2,32 +2,9 @@ package main
 
 import (
 	"backend/services"
-	"fmt"
 	"log"
 	"net/http"
 )
-
-type Task interface {
-	DoTask(s string) string
-}
-
-type TaskFunction func(s string) string
-
-func (t TaskFunction) DoTask(s string) string {
-	return t(s)
-}
-
-func DoTask1(s string) string {
-	return s + " from task 1"
-}
-
-func DoTask2(s string) string {
-	return s + " from task 2"
-}
-
-func ExecuteTask(t Task) {
-	fmt.Println(t.DoTask("Hello"))
-}
 
 func main() {
 
