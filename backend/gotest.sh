8356f6c4f1c32675f290c190d61de420872d8b1a
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-21 11:16:43 -0400

    edit the script so that I don't have to source it each time

--- backend/gotest.sh
+++ backend/gotest.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# Define your environment variables here
+export TEST_ENV=1
+
 gotest() {
-  go test $* | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | GREP_COLOR="01;33" egrep --color=always '\s*[a-zA-Z0-9\-_.]+[:][0-9]+[:]|^'
+  go test "$@" | sed -e "s/PASS/$(printf "\033[32mPASS\033[0m")/g" \
+                     -e "s/FAIL/$(printf "\033[31mFAIL\033[0m")/g" \
+                     | GREP_COLOR="01;33" egrep --color=always '\s*[a-zA-Z0-9\-_.]+[:][0-9]+[:]|^'
 }
+
+# If the script is executed directly, run gotest with provided arguments
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    gotest "$@"
+fi
