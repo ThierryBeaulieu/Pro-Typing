bf9964318160e9b49667ff62cc0a717b3c932f07
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-21 12:01:17 -0400

    the test now pass, the information is the same

--- backend/server_test.go
+++ backend/server_test.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"backend/models"
-	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
 )
 
-func getStubCertification() models.Certification {
-	var stubCertification string = `{
-		"id": "d1181969-6ae4-4a2f-9bb7-4e692aa278e7",
-		"name": "Average Typist",
-		"description": "This range includes 40-50% of all people. This certification ensures that you are typing as fast as the average person.",
-		"range": "40-55 words per minute",
-		"img": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
-	  }`
+type DatabaseStub struct {
+}
 
-	var certification models.Certification
-	json.Unmarshal([]byte(stubCertification), &certification)
-	return certification
+func (d *DatabaseStub) FetchCertification(ID string) *models.Certification {
+	return nil
+}
+
+func (d *DatabaseStub) FetchAllCertifications() []models.Certification {
+
+	certification1 := models.Certification{ID: "d1181969-6ae4-4a2f-9bb7-4e692aa278e7",
+		Name:        "Average Typist",
+		Description: "This range includes 40-50% of all people. This certification ensures that you are typing as fast as the average person.",
+		Range:       "40-55 words per minute",
+		Img:         "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="}
+	return []models.Certification{certification1}
+}
+
+func (d *DatabaseStub) FetchAllCertificationsCompleted() []models.CertificationComplete {
+	return nil
 }
 
 func TestCertificationServer(t *testing.T) {
@@ -28,32 +35,16 @@ func TestCertificationServer(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/certifications", nil)
 		response := httptest.NewRecorder()
 
-		var server CertificationServer
+		var database DatabaseStub
+
+		server := CertificationServer{&database}
 		server.ServeHTTP(response, request)
 
 		got := response.Body.String()
+		want := `[{"id": "d1181969-6ae4-4a2f-9bb7-4e692aa278e7", "name": "Average Typist", "description": "This range includes 40-50% of all people. This certification ensures that you are typing as fast as the average person.", "range": "40-55 words per minute", "img": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="}]`
 
-		want := `[{
-			"id": "d1181969-6ae4-4a2f-9bb7-4e692aa278e7",
-			"name": "Average Typist",
-			"description": "This range includes 40-50% of all people. This certification ensures that you are typing as fast as the average person.",
-			"range": "40-55 words per minute",
-			"img": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
-		},
-		{
-			"id": "11a26b4c-2795-4621-8e65-e16dfa2ff989",
-			"name": "Certified Typist",
-			"description": "This range includes 25-30% of people. At this level, you are faster than the majority but not yet at the professional level.",
-			"range": "60-75 words per minute",
-			"img": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
-		},
-		{
-			"id": "7552a1fc-c2eb-4a26-b11b-8565c5cbc583",
-			"name": "Proficient Typist",
-			"description": "This range includes about 15-20% of people. You are well above average and typing at a proficient speed.",
-			"range": "80-85 words per minute",
-			"img": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
-		}]`
+		got = strings.Join(strings.Fields(got), "")
+		want = strings.Join(strings.Fields(want), "")
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %q, want %q", got, want)
