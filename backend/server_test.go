01a3eb8efc991d19ff1606988eeb443312f2e271
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-21 10:14:21 -0400

    add a test that doesn't pass, the test is used to mimic what we want from the server

--- backend/server_test.go
+++ backend/server_test.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"backend/models"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
+
+func getStubCertification() models.Certification {
+	var stubCertification string = `{
+		"id": "d1181969-6ae4-4a2f-9bb7-4e692aa278e7",
+		"name": "Average Typist",
+		"description": "This range includes 40-50% of all people. This certification ensures that you are typing as fast as the average person.",
+		"range": "40-55 words per minute",
+		"img": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
+	  }`
+
+	var certification models.Certification
+	json.Unmarshal([]byte(stubCertification), &certification)
+	return certification
+}
 
 func TestCertificationServer(t *testing.T) {
 	t.Run("returns list of available certifications", func(t *testing.T) {
@@ -13,10 +30,14 @@ func TestCertificationServer(t *testing.T) {
 
 		CertificationServer(response, request)
 
-		got := response.Body.String()
-		want := ""
+		var certifications []models.Certification
+		json.Unmarshal([]byte(response.Body.Bytes()), &certifications)
+		got := certifications
 
-		if got != want {
+		var certification1 models.Certification = getStubCertification()
+		want := []models.Certification{certification1}
+
+		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
