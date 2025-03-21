ec6c9a1fa832b5110e657addab32a6a9da51e242
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-21 11:45:01 -0400

    code a certification string

--- backend/server.go
+++ backend/server.go
@@ -5,5 +5,13 @@ import (
 )
 
 func CertificationServer(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("[]"))
+
+	var certifications string = `"[{
+			"id": "d1181969-6ae4-4a2f-9bb7-4e692aa278e7",
+			"name": "Average Typist",
+			"description": "This range includes 40-50% of all people. This certification ensures that you are typing as fast as the average person.",
+			"range": "40-55 words per minute",
+			"img": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
+		}]"`
+	w.Write([]byte(certifications))
 }
--- backend/server_test.go
+++ backend/server_test.go
@@ -32,7 +32,13 @@ func TestCertificationServer(t *testing.T) {
 
 		got := response.Body.String()
 
-		want := "["
+		want := `"[{
+			"id": "d1181969-6ae4-4a2f-9bb7-4e692aa278e7",
+			"name": "Average Typist",
+			"description": "This range includes 40-50% of all people. This certification ensures that you are typing as fast as the average person.",
+			"range": "40-55 words per minute",
+			"img": "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII="
+		}]"`
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %q, want %q", got, want)
