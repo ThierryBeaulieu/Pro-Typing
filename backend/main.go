59543c663736a33d30ba99dc29e240411f8813ad
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-03-21 00:34:30 -0400

    create a test for having a function called CertificationServer

--- backend/main.go
+++ backend/main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
-
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
 func main() {
 
-	var task1 Task = TaskFunction(DoTask1)
-	var task2 Task = TaskFunction(DoTask2)
-	ExecuteTask(task1)
-	ExecuteTask(task2)
-
-	var memory InMemoryPlayerStore = InMemoryPlayerStore{}
-	server := &PlayerServer{&memory}
-	log.Fatal(http.ListenAndServe(":5001", server))
 }
--- backend/server_test.go
+++ backend/server_test.go
@@ -1,65 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-type StubPlayerStore struct {
-	scores map[string]int
-}
-
-func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score := s.scores[name]
-	return score
-}
-
 func TestCertificationServer(t *testing.T) {
-
-	store := StubPlayerStore{
-		map[string]int{
-			"Pepper": 20,
-			"Floyd":  10,
-		},
-	}
-
-	server := &PlayerServer{&store}
-
-	t.Run("returns Pepper's score", func(t *testing.T) {
-		request := newScoreRequest("Pepper")
+	t.Run("returns list of available certifications", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/certifications", nil)
 		response := httptest.NewRecorder()
 
-		server.ServeHTTP(response, request)
+		CertificationServer(response, request)
 
 		got := response.Body.String()
-		want := "20"
+		want := ""
 
-		assertResponseBody(t, got, want)
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	})
-
-	t.Run("returns Floyd's score", func(t *testing.T) {
-		request := newScoreRequest("Floyd")
-		response := httptest.NewRecorder()
-
-		server.ServeHTTP(response, request)
-
-		got := response.Body.String()
-		want := "10"
-
-		assertResponseBody(t, got, want)
-	})
-}
-
-func newScoreRequest(name string) *http.Request {
-	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
-	return request
-}
-
-func assertResponseBody(t testing.TB, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("response body is wrong, got %q want %q", got, want)
-	}
 }
