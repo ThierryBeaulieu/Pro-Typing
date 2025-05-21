f4c656b582f0549fd222723b3db0dea7175529c0
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-05-21 19:43:22 -0400

    allow certification controller to return all results

--- backendv2/backend/backend/Controllers/CertificationController.cs
+++ backendv2/backend/backend/Controllers/CertificationController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -15,16 +16,24 @@ namespace backend.Controllers
         [HttpGet]
         public IEnumerable<Certification> Get()
         {
-            Certification certification = new()
+
+            var options = new JsonSerializerOptions
             {
-                ID = "d1181969-6ae4-4a2f-9bb7-4e692aa278e7",
-                Name = "Average Typist",
-                Description = "This range includes 40-50% of all people. This certification ensures that you are typing as fast as the average person.",
-                Range = "40-55 words per minute",
-                ImgID = "1e0c8f97-9ec2-4353-b8eb-482b2a45c9c5"
+                PropertyNameCaseInsensitive = true
             };
 
-            return new Certification[] { certification };
+            string databasePath = "Database/database.json";
+
+            if (!System.IO.File.Exists(databasePath))
+            {
+                // Optionally return an empty list or throw an error
+                return new List<Certification>();
+            }
+
+            string databaseJson = System.IO.File.ReadAllText(databasePath);
+            List<Certification>? certifications = JsonSerializer.Deserialize<List<Certification>>(databaseJson, options);
+
+            return certifications ?? new List<Certification>();
         }
 
         // GET api/certification/6b18c787-e28b-4bc2-abea-3899a1fa5da5
