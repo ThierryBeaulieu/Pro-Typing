6451ff5062ff2ab503ef74cc25824aeded117a6d
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-05-31 17:17:52 -0400

    add test so that I can verify the database

--- backend/APITests/Controllers/CertificationControllerTests.cs
+++ backend/APITests/Controllers/CertificationControllerTests.cs
@@ -1,16 +1,21 @@
 ﻿namespace APITests;
+using backend.Controllers;
+using backend.Schemas;
 
 public class CertificationControllerTests
 {
+    private CertificationController _controller;
 
     public CertificationControllerTests()
     {
+        _controller = new CertificationController();
     }
 
     [Fact]
     public void Test1()
     {
-
-
+        IEnumerable<Certification> items = _controller.Get();
+        Assert.NotNull(items);
+        Assert.Equal(9, items.Count());
     }
 }
