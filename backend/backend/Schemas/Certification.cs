dcb2903b617abb6ede1da1fd7b5d282c499e3a55
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-05-31 16:42:23 -0400

    reinit backend v2 for C#

---
+++ backend/backend/Schemas/Certification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Schemas
+{
+    public class Certification
+    {
+        [Required]
+        public string ID { get; set; } = string.Empty;
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string Range { get; set; } = string.Empty;
+
+        [Required]
+        public string ImgID { get; set; } = string.Empty;
+
+    }
+}
